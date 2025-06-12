920f9d06aad66d00b83f0db86f6c613f0518282f
Gabriel Roque <gabrielroquegr16@gmail.com>
2025-06-12 10:16:40 -0300

    Primeiro commit
    Co-authored-by: paulin59D <paulin59D@users.noreply.github.com>
    Co-authored-by: GabrielXTsupra <GabrielXTsupra@users.noreply.github.com>
    Co-authored-by: Corinthios <Corinthios@users.noreply.github.com>

---
+++ app.js
@@ -0,0 +1,41 @@
+import express from 'express';
+import cors from 'cors';
+import { testarConexao } from './db.js';
+import swaggerUi from 'swagger-ui-express';
+import swaggerSpec from './Swagger.js';
+
+// Importações de rotas
+import RotasUsuarios, {autenticarToken} from './src/routes/RotasUsuarios.js';
+import RotasEventos from './src/routes/RotasEvento.js';
+
+
+const app = express();
+testarConexao();
+app.use(cors());
+app.use(express.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+ app.get('/', (req, res) => {
+    res.redirect("/api-docs");
+});
+
+
+// Rotas de Usuários
+app.post('/usuarios', RotasUsuarios.novoUsuario);
+app.get('/usuarios', autenticarToken, RotasUsuarios.listarUsuario);
+app.put('/usuarios/:id', autenticarToken, RotasUsuarios.atualizarUsuario);
+app.delete('/usuarios/:id', autenticarToken, RotasUsuarios.deletarUsuario);
+app.post('/usuarios/login', RotasUsuarios.login);
+app.patch('/usuarios/:id', autenticarToken, RotasUsuarios.editar);
+app.get('/usuarios/filtrarUsuario', RotasUsuarios.filtrarUsuario);
+
+
+//Rotas de Eventos
+ app.post('/eventos', RotasEventos.cadastrar);
+
+
+const porta = 3000;
+app.listen(porta, () => {
+    console.log(`API rodando em: http://localhost:${porta}`);
+});
+
